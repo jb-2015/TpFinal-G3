8a7e828f1ab164abd1360598a1592467d7d1a21d
jb-2015 <juan_matias_becerra@hotmail.com>
2023-06-06 12:29:19 -0300

    Parte de la estructura de archivos

--- src/modelo/Comentario.java
+++ src/modelo/Comentario.java
@@ -16,6 +16,55 @@ public class Comentario {
     private String comentario;
     private LocalDate fecha_avance;
     private Tarea tarea;
+
+    public Comentario() {
+    }
+
+    public Comentario(int idComentario, String comentario, LocalDate fecha_avance, Tarea tarea) {
+        this.idComentario = idComentario;
+        this.comentario = comentario;
+        this.fecha_avance = fecha_avance;
+        this.tarea = tarea;
+    }
+
+    public Comentario(String comentario, LocalDate fecha_avance, Tarea tarea) {
+        this.comentario = comentario;
+        this.fecha_avance = fecha_avance;
+        this.tarea = tarea;
+    }
+
+    public int getIdComentario() {
+        return idComentario;
+    }
+
+    public void setIdComentario(int idComentario) {
+        this.idComentario = idComentario;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public LocalDate getFecha_avance() {
+        return fecha_avance;
+    }
+
+    public void setFecha_avance(LocalDate fecha_avance) {
+        this.fecha_avance = fecha_avance;
+    }
+
+    public Tarea getTarea() {
+        return tarea;
+    }
+
+    public void setTarea(Tarea tarea) {
+        this.tarea = tarea;
+    }
+    
     
     
 }
