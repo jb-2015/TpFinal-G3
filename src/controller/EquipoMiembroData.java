ca60575ca3c98e3d46a9b3917fc5faea0b45e9c1
calderon104 <gonza.rc1004@gmail.com>
2023-06-07 19:20:47 -0300

    Update EquipoMiembroData.java

--- src/controller/EquipoMiembroData.java
+++ src/controller/EquipoMiembroData.java
@@ -5,6 +5,11 @@
  */
 package controller;
 
+import com.mysql.cj.xdevapi.PreparableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 import modelo.EquipoMiembro;
 
 /**
@@ -13,10 +18,26 @@ import modelo.EquipoMiembro;
  */
 public class EquipoMiembroData {
     private MiembroData md= new MiembroData();
+    //private EquipoData ed= new EquipoData();
     //EquipoData ed= new EquipoData();
     
     public EquipoMiembro buscarEquipoMiembro(int id){
-        
+        String sql = "SELECT * FROM equipomiembros WHERE idMiembroEq=?";
+         EquipoMiembro em = null;
+        try {
+            PreparedStatement ps = Conexion.conectar().prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs=ps.executeQuery();//devolviendo todos los equipos.
+            if(rs.next()){
+            em =new EquipoMiembro();
+            em.setIdEquipoMiembro(rs.getInt("idMiembroEq"));
+            em.setFecha_incorporacion(rs.getDate("fecha_incorporacion").toLocalDate());
+            em.setMiembro(md.buscarMiembro(rs.getInt("idMiembro")));
+            //em.setEquipo(ed.buscarEquipo(rs.getInt(idEquipo)));
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "ERROR AL BUSCAR EQUIPO MIEMBRO"+ e.getMessage());
+        }
         return null;
     }
     
