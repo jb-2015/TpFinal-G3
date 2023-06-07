8a58f2c31bd363561a2be376c881f69ec5333bf8
jb-2015 <juan_matias_becerra@hotmail.com>
2023-06-07 20:24:20 -0300

    SET importacion de ConsultaPorEstado
    UPDATE TareaData

--- src/controller/EquipoData.java
+++ src/controller/EquipoData.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
-import controller.Estados;
+import controller.ConsultaPorEstados;
 
 
 
