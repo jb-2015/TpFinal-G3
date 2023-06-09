1483e763180e432970665cd85d2a9c317d28260c
calderon104 <gonza.rc1004@gmail.com>
2023-06-09 18:35:59 -0300

    Update EquipoMiembroData.java

--- src/controller/EquipoMiembroData.java
+++ src/controller/EquipoMiembroData.java
@@ -21,39 +21,41 @@ import modelo.EquipoMiembro;
  * @author juany
  */
 public class EquipoMiembroData {
-    private MiembroData md= new MiembroData();
+
+    private MiembroData md = new MiembroData();
     private EquipoData ed = new EquipoData();
     //private EquipoData ed= new EquipoData();
     //EquipoData ed= new EquipoData();
-    
-    public EquipoMiembro buscarEquipoMiembro(int id){
+
+    public EquipoMiembro buscarEquipoMiembro(int id) {
         String sql = "SELECT * FROM equipomiembros WHERE idMiembroEq=?";
-         EquipoMiembro em = null;
+        EquipoMiembro em = null;
         try {
             PreparedStatement ps = Conexion.conectar().prepareStatement(sql);
             ps.setInt(1, id);
-            ResultSet rs=ps.executeQuery();//devolviendo todos los equipos.
-            if(rs.next()){
-            em =new EquipoMiembro();
-            em.setIdEquipoMiembro(rs.getInt("idMiembroEq"));
-            em.setFecha_incorporacion(rs.getDate("fecha_incorporacion").toLocalDate());
-            em.setMiembro(md.buscarMiembro(rs.getInt("idMiembro")));
-            //em.setEquipo(ed.buscarEquipo(rs.getInt(idEquipo)));
+            ResultSet rs = ps.executeQuery();//devolviendo todos los equipos.
+            if (rs.next()) {
+                em = new EquipoMiembro();
+                em.setIdEquipoMiembro(rs.getInt("idMiembroEq"));
+                em.setFecha_incorporacion(rs.getDate("fecha_incorporacion").toLocalDate());
+                em.setMiembro(md.buscarMiembro(rs.getInt("idMiembro")));
+                //em.setEquipo(ed.buscarEquipo(rs.getInt(idEquipo)));
             }
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, "ERROR AL BUSCAR EQUIPO MIEMBRO"+ e.getMessage());
+            JOptionPane.showMessageDialog(null, "ERROR AL BUSCAR EQUIPO MIEMBRO" + e.getMessage());
         }
         return null;
     }
-    public ArrayList<EquipoMiembro> listarEquipoMiembro(){
+
+    public ArrayList<EquipoMiembro> listarEquipoMiembro() {
         ArrayList<EquipoMiembro> arrEM = new ArrayList();
         String sql = "SELECT * FROM equipomiembros WHERE estado = 1";
-        
-        try{
+
+        try {
             PreparedStatement ps = Conexion.conectar().prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
-            
-            while(rs.next()){
+
+            while (rs.next()) {
                 EquipoMiembro em = new EquipoMiembro();
                 em.setIdEquipoMiembro(rs.getInt("idMiembroEq"));
                 em.setFecha_incorporacion(rs.getDate("fecha_incorporacion").toLocalDate());
@@ -62,15 +64,15 @@ public class EquipoMiembroData {
                 em.setEstado(rs.getBoolean("estado"));
                 arrEM.add(em);
             }
-                
-        }catch(SQLException e){
-            JOptionPane.showMessageDialog(null,"Error al listar Equipo/miembro");
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al listar Equipo/miembro");
         }
-        
+
         return arrEM;
     }
-    
-     public void guardarEquipoMiembro(EquipoMiembro em) {
+
+    public void guardarEquipoMiembro(EquipoMiembro em) {
         String sql = "INSERT INTO equipomiembros(fecha_incorporacion, idEquipo,idMiembro,estado) VALUES (?, ?, ?, ?)";
         try {
             PreparedStatement ps = Conexion.conectar().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS); //DEVUEL CON SU KEY 
@@ -90,5 +92,20 @@ public class EquipoMiembroData {
             JOptionPane.showMessageDialog(null, "Error al acceder a la Tabla Equipo-miembro " + ex.getMessage());
         }
     }
-    
+
+    public void borrarEquipoMiembro(int id) {
+        String sql = "UPDATE * FROM equipomiembros SET estado=0 WHERE idMiembroEq=?";
+        try {
+            PreparedStatement ps = Conexion.conectar().prepareStatement(sql);
+            ps.setInt(1, id);
+            int res = ps.executeUpdate();
+            if (res == 1) {
+                JOptionPane.showMessageDialog(null, "Equipo miembro eliminado");
+            }else{
+            JOptionPane.showMessageDialog(null, "NO se pudo eliminar el miembro equipo");}
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al borrar EQUIPO MIEMBRO"+ e.getMessage());
+        }
+    }
+
 }
